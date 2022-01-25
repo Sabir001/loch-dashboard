ea2f4efea43fe5e8db6974006822ff118b33e25e
sabir001 <asabir24@gmail.com>
2022-01-25 13:24:20 +0600

    Added sign in form

--- src/components/Auth/SigninForm.js
+++ src/components/Auth/SigninForm.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "./SigninForm.css";
 
-function SigninForm() {
-  return <div>Todo</div>;
+const validEmailRegex = RegExp(
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
+
+function SigninForm({ userSignIn, signinLoader }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email) {
+      toast.error("Please enter your email");
+    } else if (!password) {
+      toast.error("Please enter your password");
+    } else if (!email.match(validEmailRegex)) {
+      toast.error("Invalid email address");
+    } else {
+      userSignIn({
+        username: email,
+        password,
+      });
+    }
+  };
+  return (
+    <div className="sign-in-form">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="name@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="primary">Log in</button>
+      </form>
+    </div>
+  );
 }
 
 export default SigninForm;
--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -33,7 +33,6 @@ function AuthLayout({ history }) {
     <>
       <Header />
       <Switch>{getRoutes(routes)}</Switch>
-      <Footer />
     </>
   );
 }
